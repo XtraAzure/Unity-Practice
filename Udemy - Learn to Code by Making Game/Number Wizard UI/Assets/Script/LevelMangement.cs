4117898d293f8f5d8835a53bd241abbbb1d180fc
XtraAzure <35952292+XtraAzure@users.noreply.github.com>
2018-04-28 01:41:26 +0900

    Add files via upload

---
+++ Udemy - Learn to Code by Making Game/Number Wizard UI/Assets/Script/LevelMangement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelMangement : MonoBehaviour {
+
+	public void LoadLevel(string name)
+    {
+        Debug.Log("Change scene" + name);
+        Application.LoadLevel(name);
+    }
+
+    public void QuitRequest()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+}
---
+++ Udemy - Learn to Code by Making Game/Number Wizard UI/Assets/Script/NumberWizards.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class NumberWizards : MonoBehaviour {
+	// Use this for initialization
+	int max;
+	int min;
+	int guess;
+
+    public int MaxGuessesAllowed = 7;
+
+    public Text text;
+
+	void Start () {
+		StartGame();
+	}
+	
+	void StartGame () {
+		max = 1000;
+		min = 1;
+        NextGuess();
+    }
+
+    public void GuessHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+
+    public void GuessLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+
+	void NextGuess () {
+        guess = Random.Range(min, max);
+        text.text = guess.ToString();
+        MaxGuessesAllowed = MaxGuessesAllowed - 1;
+        if (MaxGuessesAllowed <=0)
+        {
+            Application.LoadLevel("Win");
+        }
+	}
+}
