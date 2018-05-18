b633723c6cc2e52c5a51c951e43abedb7c12f7d0
XtraAzure <35952292+XtraAzure@users.noreply.github.com>
2018-05-19 01:32:34 +0900

    Upload Laser Defender
    Using git program since it has more than 100 files and has more than 25mb files

---
+++ Udemy - Learn to Code by Making Game/Laser Defender/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public GameObject laser;
+    public float projectileSpeed = 10;
+    public float projectileRepeatRate = 0.2f;
+
+    public float speed = 15.0f;
+    public float padding = 1;
+	public float health = 200;
+
+    public AudioClip fireSound;
+
+    private float xmax = -5;
+    private float xmin = 5;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        Projectile missile = collider.gameObject.GetComponent<Projectile>();
+               if (missile)
+        {
+            health -= missile.GetDamage();
+            missile.Hit();
+                       if (health <= 0)
+            {
+                Die();
+                           }
+                   }
+           }	
+	void Die()
+    {
+        Destroy(gameObject);
+        LevelManager man = GameObject.Find("LevelManager").GetComponent<LevelManager>();
+        man.LoadLevel("Win Screen");
+    }
+	
+ 	void Start()
+    {
+        Camera camera = Camera.main;
+        float distance = transform.position.z - camera.transform.position.z;
+        xmin = camera.ViewportToWorldPoint(new Vector3(0, 0, distance)).x + padding;
+        xmax = camera.ViewportToWorldPoint(new Vector3(1, 1, distance)).x - padding;
+    }
+
+    void Fire()
+    {
+        //Vector3 Space = new Vector3(0, 0.6f, 0);
+        GameObject beam = Instantiate(laser, transform.position, Quaternion.identity) as GameObject;
+        beam.GetComponent<Rigidbody2D>().velocity = new Vector3(0, projectileSpeed, 0);
+        AudioSource.PlayClipAtPoint(fireSound, transform.position);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            InvokeRepeating("Fire", 0.0001f, projectileRepeatRate);
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            CancelInvoke("Fire");
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position = new Vector3(
+                Mathf.Clamp(transform.position.x - speed * Time.deltaTime, xmin, xmax),
+                transform.position.y,
+                transform.position.z
+            );
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position = new Vector3(
+                Mathf.Clamp(transform.position.x + speed * Time.deltaTime, xmin, xmax),
+                transform.position.y,
+                transform.position.z
+            );
+        }
+    }
+}
